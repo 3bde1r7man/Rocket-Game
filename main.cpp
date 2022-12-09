a3261b3a4d4a03f2f796809cb19307ab997a10fd
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2022-12-09 15:58:26 +0200

    add AI but i can win

--- main.cpp
+++ main.cpp
@@ -3,5 +3,21 @@
 
 int main() {
    RocketGame rocketGame;
-   rocketGame.run();
+   int ch;
+   while (true)
+   {
+	   cout << "1- play with onther human\n2- play with computer\n--> ";
+	   cin >> ch;
+	   switch (ch)
+	   {
+	   case 1:
+		   rocketGame.runHuman();
+		   exit(0);
+	   case 2:
+		   rocketGame.runPC();
+		   exit(0);
+	   default:
+		   break;
+	   }
+   }
 }
